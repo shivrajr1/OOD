067135bca0d4b3b0971ceda3d99c52b98d6936d5
shivraj <shivrajrajbhar9319@gmail.com>
2026-06-30 17:30:09 +0530

    command pattern

---
+++ command/Light.java
@@ -0,0 +1,8 @@
+public class Light {
+    public void turnOff(){
+        System.out.println("light turned off");
+    }
+    public void turnOn(){
+        System.out.println("light turned on");
+    }
+}
